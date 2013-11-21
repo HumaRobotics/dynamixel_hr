3f553a5ad63598bcaad8e45a27c27e1c458149ce
Philippe Capdepuy <pc@generationrobots.com>
2013-11-21 18:23:34 +0100

    added ros binding and test, publishes position, subscribes to enable

---
+++ TestROS.py
@@ -0,0 +1,17 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+
+import roslib
+import rospy
+from dxl import *
+from dxl import dxlros
+
+
+if __name__=="__main__":
+        rospy.init_node("dxlros")
+        chain=dxlchain.DxlChain("/dev/ttyUSB0",rate=3000000)
+        chain.get_motor_list()
+        bindings={1:"pan",2:"tilt1",3:"tilt2",4:"tilt3",5:"head"}
+        dxlros=dxlros.DxlROS(chain,bindings)        
+        rospy.spin()
---
+++ dxl/dxlros.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+# Dynamixel library for MX28 and MX64
+
+# WINDOWS WARNING: For best performance, parameters of the COM Port should be set to maximum baud rate, and 1ms delay (Device Manager, COM Ports, properties, advanced)
+
+        
+import roslib
+import rospy
+from threading import Thread
+
+from sensor_msgs.msg import *
+from std_msgs.msg import *
+
+class DxlROS(Thread):
+    def __init__(self,chain,bindings):
+        Thread.__init__(self)
+        self.chain=chain
+        self.bindings=bindings
+        for (id,name) in self.bindings.items():
+            if id not in self.chain.motors.keys():
+                raise Exception,"Cannot bind ROS name %s to non-existing motor id %d"%(name,id)
+        rospy.Subscriber("/dxl/enable",Bool,self.enable)
+        self.buildPublishers()
+        self.start()
+    
+    def run(self):
+        r=rospy.Rate(10)
+        while not rospy.is_shutdown():
+            for (id,name) in self.bindings.items():
+                v=self.chain.get_reg(id,"present_position")
+                self.pub[id].publish(v)            
+            r.sleep()
+        
+        
+        
+    
+    def buildPublishers(self):
+        self.pub={}
+        for (id,name) in self.bindings.items():
+            self.pub[id]=rospy.Publisher("/dxl/%s/position"%name,Int16)
+            
+            
+        
+    def enable(self,msg):
+        print msg.data
+        if msg.data:
+            print "Got enable message"
+        else:
+            print "Got disable message"
+        self.chain.enable(msg.data)
+            
+        
+
+        
