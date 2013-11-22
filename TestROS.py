c14e3889e0df160e60c6b4a56301428ea3bd521f
Philippe Capdepuy <pc@generationrobots.com>
2013-11-22 13:30:15 +0100

    added dxlros raw bindings

--- TestROS.py
+++ TestROS.py
@@ -9,9 +9,9 @@ from dxl import dxlros
 
 
 if __name__=="__main__":
-        rospy.init_node("dxlros")
+        rospy.init_node("dxl")
         chain=dxlchain.DxlChain("/dev/ttyUSB0",rate=1000000)
         chain.get_motor_list()
         bindings={1:"pan",2:"tilt1",3:"tilt2",4:"tilt3",5:"head"}
-        dxlros=dxlros.DxlROS(chain,bindings)        
+        dxlros=dxlros.DxlROS(chain,rate=1,bindings=bindings)        
         rospy.spin()
--- dxl/dxlros.py
+++ dxl/dxlros.py
@@ -5,6 +5,7 @@
 # WINDOWS WARNING: For best performance, parameters of the COM Port should be set to maximum baud rate, and 1ms delay (Device Manager, COM Ports, properties, advanced)
 
         
+import logging
 import roslib
 import rospy
 from threading import Thread
@@ -13,10 +14,12 @@ from sensor_msgs.msg import *
 from std_msgs.msg import *
 
 class DxlROS(Thread):
-    def __init__(self,chain,bindings=None):
+    def __init__(self,chain,rate=10,bindings=None,raw=True):
         Thread.__init__(self)
-        self.chain=chain        
+        self.chain=chain
+        self.rate=rate
         self.bindings=bindings
+        self.raw=raw
         
         if self.bindings==None: # Use IDs as names if no binding provided
             b={}
@@ -27,16 +30,27 @@ class DxlROS(Thread):
         for (id,name) in self.bindings.items():
             if id not in self.chain.motors.keys():
                 raise Exception,"Cannot bind ROS name %s to non-existing motor ID %d"%(name,id)
+
+        logging.info("Creating ROS elements")
+        
         rospy.Subscriber("/dxl/enable",Bool,self.enable)
+        
         self.buildPublishers()
+        self.buildSubscribers()
+        
         self.start()
     
     def run(self):
-        r=rospy.Rate(10)
+        r=rospy.Rate(float(self.rate))
         while not rospy.is_shutdown():
-            for (id,name) in self.bindings.items():
-                v=self.chain.get_reg(id,"present_position")
-                self.pub[id].publish(v)            
+            if self.raw:
+                for id,motorname in self.bindings.items():
+                    regs=self.chain.motors[id].registers
+                    basename="/dxl/%s/"%motorname
+                    for regname,reg in regs.items():
+                        topic=basename+regname
+                        v=self.chain.get_reg(id,regname)
+                        self.pub[topic].publish(v)            
             r.sleep()
         
         
@@ -44,14 +58,36 @@ class DxlROS(Thread):
     
     def buildPublishers(self):
         self.pub={}
-        for (id,name) in self.bindings.items():
-            self.pub[id]=rospy.Publisher("/dxl/%s/position"%name,Int16)
+        print("Creating ROS Publishers")
+        if self.raw:
+            for id,motorname in self.bindings.items():            
+                regs=self.chain.motors[id].registers
+                basename="/dxl/%s/"%motorname
+                for regname,reg in regs.items():
+                    topic=basename+regname
+                    print("Creating ROS Publisher: %s"%topic)
+                    self.pub[topic]=rospy.Publisher(topic,Int32)
+
+    def buildSubscribers(self):
+        if self.raw:
+            for id,motorname in self.bindings.items():            
+                regs=self.chain.motors[id].registers
+                basename="/dxl/%s/"%motorname
+                for regname,reg in regs.items():
+                    if 'w' in reg.mode:
+                        topic=basename+regname+"/set"
+                        print("Creating ROS Subscriber: %s"%topic)
+                        rospy.Subscriber(topic,Int32,lambda msg,id=id,regname=regname: self.set_register(msg,id,regname) )
+            
             
             
         
     def enable(self,msg):
         self.chain.enable(msg.data)
             
-        
+    def set_register(self,msg,id,regname):
+        v=msg.data
+        print "ros call ID %d reg %s val %d"%(id,regname,v)
+        self.chain.set_reg(id,regname,v)
 
         
